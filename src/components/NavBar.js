5c489a84b9f8dbb904e11dea5123ccee4685aa5e
FARACE cristian <cristianfarace@gmail.com>
2022-07-03 22:47:57 -0300

    Hice la llamada por Fetch... ahora me queda el resto

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand text-uppercase " href="/">
+          Rick and Morty App 
+          {/* minuto 11 y 22 hizo algo con respecto a esto  */}
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
